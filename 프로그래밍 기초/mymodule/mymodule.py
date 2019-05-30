7227e2f5d6b0143960bec34483a70c279c956be4
yujun0621 <yujun0621@naver.com>
2019-05-30 17:52:33 +0900

    5/30

---
+++ 프로그래밍 기초/mymodule/mymodule.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+def run():
+    print("시작 되었습니다")
+    
+def end():
+    print("시작 되었습니다")
+    
+def calc_sum(a,b):
+    return a+b
+
+if __name__ == "__main__":
+    run()
+    end()
---
+++ 프로그래밍 기초/mymodule/runfile.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import mymodule1
+
+if __name__ == "__main__":
+    total = mymodule1.calc_sum(5,6)
+    print(total)
